f35e86e6396f8e4a94f7d9b74c442676e039035a
sertchan <ardaseyhan@proton.me>
2026-07-27 11:38:59 +0300

    treewide: switch monospace font to adwaita mono

--- homes/seyhan/programs/alacritty/default.nix
+++ homes/seyhan/programs/alacritty/default.nix
@@ -6,25 +6,25 @@ _: {
       window.opacity = 0.90;
 
       font = {
-        size = 10;
+        size = 12;
 
         normal = {
-          family = "Jetbrains Mono NF";
+          family = "AdwaitaMono Nerd Font";
           style = "Regular";
         };
 
         bold = {
-          family = "Jetbrains Mono NF";
+          family = "AdwaitaMono Nerd Font";
           style = "Bold";
         };
 
         italic = {
-          family = "Jetbrains Mono NF";
+          family = "AdwaitaMono Nerd Font";
           style = "Italic";
         };
 
         bold_italic = {
-          family = "Jetbrains Mono NF";
+          family = "AdwaitaMono Nerd Font";
           style = "Bold Italic";
         };
       };

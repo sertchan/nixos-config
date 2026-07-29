4d93ca49c65eab946ff4becbd7606790aac62f35
sertchan <ardaseyhan@proton.me>
2026-07-29 21:12:24 +0300

    homes/seyhan: replace alacritty with ghostty

---
+++ homes/seyhan/programs/ghostty/default.nix
@@ -0,0 +1,42 @@
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      background-opacity = 1.0;
+
+      font-size = 12;
+      font-family = "AdwaitaMono Nerd Font";
+      font-family-bold = "AdwaitaMono Nerd Font";
+      font-family-italic = "AdwaitaMono Nerd Font";
+      font-family-bold-italic = "AdwaitaMono Nerd Font";
+
+      background = "0f0f0f";
+      foreground = "c5c9c5";
+
+      selection-background = "2d4f67";
+      selection-foreground = "c8c093";
+
+      palette = [
+        "0=#0c0b0b"
+        "1=#c4746e"
+        "2=#8a9a7b"
+        "3=#c4b28a"
+        "4=#8ba4b0"
+        "5=#a292a3"
+        "6=#8ea4a2"
+        "7=#C8C093"
+        "8=#a6a69c"
+        "9=#E46876"
+        "10=#87a987"
+        "11=#E6C384"
+        "12=#7FB4CA"
+        "13=#938AA9"
+        "14=#7AA89F"
+        "15=#c5c9c5"
+
+        "16=#ffa066"
+        "17=#ff5d62"
+      ];
+    };
+  };
+}

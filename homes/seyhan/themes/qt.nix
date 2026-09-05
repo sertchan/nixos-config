93b9c25cb59f5c70d8f5900be01f467174ee18b2
sertchan <ardaseyhan@proton.me>
2026-09-06 01:35:39 +0300

    homes/seyhan: theme qt with breeze dark

--- homes/seyhan/themes/qt.nix
+++ homes/seyhan/themes/qt.nix
@@ -1,7 +1,45 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  breeze = pkgs.kdePackages.breeze;
+
+  kdeglobals = pkgs.concatText "kdeglobals" [
+    "${breeze}/share/color-schemes/BreezeDark.colors"
+    (pkgs.writeText "kdeglobals-extra" ''
+
+      [KDE]
+      widgetStyle=Breeze
+
+      [Icons]
+      Theme=breeze-dark
+
+      [General]
+      font=Adwaita Sans,11,-1,5,50,0,0,0,0,0
+      menuFont=Adwaita Sans,11,-1,5,50,0,0,0,0,0
+      toolBarFont=Adwaita Sans,11,-1,5,50,0,0,0,0,0
+      smallestReadableFont=Adwaita Sans,9,-1,5,50,0,0,0,0,0
+      fixed=Adwaita Mono,11,-1,5,50,0,0,0,0,0
+    '')
+  ];
+in
+{
   qt = {
     enable = true;
-    style.name = "adwaita-dark";
-    style.package = pkgs.adwaita-qt;
+
+    platformTheme = {
+      name = "kde";
+      package = with pkgs.kdePackages; [
+        plasma-integration
+        kio
+      ];
+    };
+
+    style = {
+      name = "Breeze";
+      package = [ breeze ];
+    };
   };
+
+  xdg.configFile."kdeglobals".source = kdeglobals;
+
+  home.packages = [ pkgs.kdePackages.breeze-icons ];
 }

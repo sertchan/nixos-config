2ff2955304c9f1379b6d0c93e02819ff04369cee
sertchan <ardaseyhan@proton.me>
2026-09-08 12:55:43 +0300

    desktop/audio: gate on sound device configuration

--- modules/desktop/audio.nix
+++ modules/desktop/audio.nix
@@ -1,11 +1,21 @@
 {
-  security.rtkit.enable = true;
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    wireplumber.enable = true;
+  dev = config.modules.device;
+in {
+  config = mkIf dev.hasSound {
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
   };
 }

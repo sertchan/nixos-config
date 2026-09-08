550f38fdccad2768551153e33109d3c32b76187d
sertchan <ardaseyhan@proton.me>
2026-09-08 12:37:31 +0300

    zsh: remove the GPG_TTY export

--- homes/seyhan/programs/zsh/default.nix
+++ homes/seyhan/programs/zsh/default.nix
@@ -1,14 +1,8 @@
 _: {
   programs.zsh = {
     enable = true;
-
     shellAliases = {
-      # Change working directory to ranger's last visited directory on exit
       ranger = ''ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'';
     };
-
-    initContent = ''
-      export GPG_TTY=$(tty) # Register active TTY for GPG passphrase prompts
-    '';
   };
 }

cddb9c481b2448fbdb279b3f7f3a6f8da3c6b97b
sertchan <ardaseyhan@proton.me>
2026-09-08 12:58:33 +0300

    hardware/bluetooth: gate on radio device configuration

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -1,34 +1,42 @@
-{ pkgs, ... }: {
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    disabledPlugins = [ "sap" ];
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
 
-    settings = {
-      General = {
-        JustWorksRepairing = "always";
-        MultiProfile = "multiple";
-        Experimental = true;
-        FastConnectable = true;
-      };
-      Policy = {
-        AutoEnable = true;
+  dev = config.modules.device;
+in {
+  config = mkIf dev.hasBluetooth {
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      disabledPlugins = ["sap"];
+      settings = {
+        General = {
+          JustWorksRepairing = "always";
+          MultiProfile = "multiple";
+          Experimental = true;
+          FastConnectable = true;
+        };
+        Policy = {AutoEnable = true;};
       };
     };
-  };
 
-  systemd.user.services.mpris-proxy = {
-    enable = true;
-    description = "MPRIS Proxy for Bluetooth devices";
-    wantedBy = [ "default.target" ];
-    after = [
-      "network.target"
-      "sound.target"
-    ];
-    serviceConfig = {
-      ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-      Restart = "on-failure";
-      RestartSec = 5;
+    systemd.user.services.mpris-proxy = {
+      enable = true;
+      description = "MPRIS Proxy for Bluetooth devices";
+      wantedBy = ["default.target"];
+      after = [
+        "network.target"
+        "sound.target"
+      ];
+      serviceConfig = {
+        ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
     };
   };
 }

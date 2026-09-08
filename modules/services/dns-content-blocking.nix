670af7442706a22ed1d13ee68e2564dbcf4ce04b
sertchan <ardaseyhan@proton.me>
2026-09-08 21:41:45 +0300

    modules/services: inherit mkForce from lib.modules

--- modules/services/dns-content-blocking.nix
+++ modules/services/dns-content-blocking.nix
@@ -1,5 +1,7 @@
-{lib, ...}: {
-  networking.nameservers = lib.mkForce [
+{lib, ...}: let
+  inherit (lib.modules) mkForce;
+in {
+  networking.nameservers = mkForce [
     "159.69.155.94#wurzn.hagezi.org"
     "2a01:4f8:1c1c:d363::1#wurzn.hagezi.org"
   ];

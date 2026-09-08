f5bb3f45d7539927ef17340dc9d57e107d2fcd38
sertchan <ardaseyhan@proton.me>
2026-09-08 21:17:16 +0300

    homes/seyhan: share gtk settings between gtk3 and gtk4

--- homes/seyhan/themes/gtk.nix
+++ homes/seyhan/themes/gtk.nix
@@ -1,51 +1,43 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  themeName = "adw-gtk3-dark";
+  iconThemeName = "Adwaita";
+
+  toolkitSettings = {
+    gtk-decoration-layout = "appmenu:none";
+    gtk-enable-event-sounds = 0;
+    gtk-enable-input-feedback-sounds = 0;
+    gtk-xft-antialias = 1;
+    gtk-xft-hinting = 1;
+    gtk-xft-hintstyle = "hintslight";
+    gtk-xft-rgba = "rgb";
+    gtk-error-bell = 0;
+    gtk-enable-primary-paste = false;
+  };
+in {
   gtk = {
     enable = true;
     theme = {
-      name = "adw-gtk3-dark";
+      name = themeName;
       package = pkgs.adw-gtk3;
     };
     iconTheme = {
-      name = "Adwaita";
+      name = iconThemeName;
       package = pkgs.adwaita-icon-theme;
     };
     font = {
       name = "Adwaita Sans";
       size = 11;
     };
-    gtk3 = {
-      extraConfig = {
-        gtk-decoration-layout = "appmenu:none";
-        gtk-enable-event-sounds = 0;
-        gtk-enable-input-feedback-sounds = 0;
-        gtk-xft-antialias = 1;
-        gtk-xft-hinting = 1;
-        gtk-xft-hintstyle = "hintslight";
-        gtk-xft-rgba = "rgb";
-        gtk-error-bell = 0;
-        gtk-enable-primary-paste = false;
-      };
-    };
+    gtk3.extraConfig = toolkitSettings;
     gtk4 = {
       theme = null;
-      extraConfig = {
-        gtk-decoration-layout = "appmenu:none";
-        gtk-enable-event-sounds = 0;
-        gtk-enable-input-feedback-sounds = 0;
-        gtk-xft-antialias = 1;
-        gtk-xft-hinting = 1;
-        gtk-xft-hintstyle = "hintslight";
-        gtk-xft-rgba = "rgb";
-        gtk-enable-primary-paste = false;
-        gtk-error-bell = 0;
-        gtk-hint-font-metrics = 1;
-      };
+      extraConfig = toolkitSettings // {gtk-hint-font-metrics = 1;};
     };
   };
 
   dconf.settings."org/gnome/desktop/interface" = {
     color-scheme = "prefer-dark";
-    gtk-theme = "adw-gtk3-dark";
-    icon-theme = "Adwaita";
+    gtk-theme = themeName;
+    icon-theme = iconThemeName;
   };
 }

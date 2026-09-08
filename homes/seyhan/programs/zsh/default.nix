b4fdaec7202d47502b9e5d9e81d3f6ba98c0119b
sertchan <ardaseyhan@proton.me>
2026-09-08 13:08:18 +0300

    homes/seyhan: remove GPG_TTY export

--- homes/seyhan/programs/zsh/default.nix
+++ homes/seyhan/programs/zsh/default.nix
@@ -1,13 +1,8 @@
 _: {
   programs.zsh = {
     enable = true;
-
     shellAliases = {
       ranger = ''ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'';
     };
-
-    initContent = ''
-      export GPG_TTY=$(tty)
-    '';
   };
 }

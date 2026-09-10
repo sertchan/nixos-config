7afc8583711228d939db988a926336ac5fdbd7f6
sertchan <ardaseyhan@proton.me>
2026-09-10 04:23:52 +0300

    homes/seyhan: remove annoying md linter feature

--- homes/seyhan/programs/neovim/plugins.nix
+++ homes/seyhan/programs/neovim/plugins.nix
@@ -9,7 +9,19 @@
     lualine-nvim
     mini-pairs
     mini-surround
-    nvim-lint
+    {
+      plugin = nvim-lint;
+      type = "lua";
+      config = ''
+        local lint = require("lint")
+
+        lint.linters["markdownlint-cli2"] = require("lint.util").wrap(lint.linters["markdownlint-cli2"], function(diagnostic)
+          if not diagnostic.message:match("^error MD013/") then
+            return diagnostic
+          end
+        end)
+      '';
+    }
     nvim-tree-lua
     nvim-web-devicons
   ];

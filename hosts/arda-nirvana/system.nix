aca464ccdd45fdf19d39936ff45752dbf8f20f8f
sertchan <ardaseyhan@proton.me>
2026-09-19 18:23:18 +0300

    hosts/arda-nirvana: change lusk disk locating method

--- hosts/arda-nirvana/system.nix
+++ hosts/arda-nirvana/system.nix
@@ -20,7 +20,7 @@ in {
         "rtsx_usb_sdmmc"
       ];
       kernelModules = [];
-      luks.devices.cryptroot.device = "/dev/disk/by-partuuid/35a50b89-1a29-49eb-bb8c-45f844d284ef";
+      luks.devices.cryptroot.device = "/dev/disk/by-uuid/f85a37d0-02ef-497d-833d-2fb4288814ae";
     };
     kernelModules = ["kvm-intel"];
     kernelParams = ["mem_sleep_default=deep"];

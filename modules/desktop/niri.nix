867fb5d767cd9db84ddb2f72213136f30d90ce07
sertchan <ardaseyhan@proton.me>
2026-09-20 11:44:40 +0300

    desktop/niri: import named environment into session

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -1,11 +1,27 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
+  inherit (lib.strings) escapeShellArg;
+
   user = config.users.users.seyhan;
+
+  importWholeEnvironment = "systemctl --user import-environment";
+  importNamedEnvironment = "${importWholeEnvironment} $(awk 'BEGIN{for (name in ENVIRON) if (name != \"AWKPATH\" && name != \"AWKLIBPATH\") print name}')";
 in {
-  programs.niri.enable = true;
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri.overrideAttrs (prev: {
+      postInstall =
+        prev.postInstall
+        + ''
+          substituteInPlace $out/bin/niri-session \
+            --replace-fail ${escapeShellArg importWholeEnvironment} ${escapeShellArg importNamedEnvironment}
+        '';
+    });
+  };
 
   environment = {
     systemPackages = with pkgs; [xwayland-satellite];

40fb9297f1de26138940a85cb3e527fc552fc366
sertchan <ardaseyhan@proton.me>
2026-09-20 13:30:52 +0300

    homes/seyhan: select firefox builtin dark theme

--- homes/seyhan/programs/firefox/interface.nix
+++ homes/seyhan/programs/firefox/interface.nix
@@ -1,4 +1,7 @@
-_: {
+_: let
+  darkAppearance = 1;
+  builtinDarkTheme = "firefox-compact-dark@mozilla.org";
+in {
   programs.firefox.policies = {
     DisplayBookmarksToolbar = "never";
     DisplayMenuBar = "never";
@@ -6,9 +9,22 @@ _: {
     FirefoxHome.TopSites = false;
     TranslateEnabled = false;
 
-    Preferences."media.videocontrols.picture-in-picture.video-toggle.enabled" = {
-      Value = false;
-      Status = "default";
+    Preferences = {
+      "media.videocontrols.picture-in-picture.video-toggle.enabled" = {
+        Value = false;
+        Status = "default";
+      };
+
+      "ui.systemUsesDarkTheme" = {
+        Value = darkAppearance;
+        Type = "number";
+        Status = "user";
+      };
+
+      "extensions.activeThemeID" = {
+        Value = builtinDarkTheme;
+        Status = "user";
+      };
     };
   };
 }
